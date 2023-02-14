a1eaa23442310f228de25c6251c90b4893d39ab7
Edgar Gomes <edgargomes.iff@gmail.com>
2023-02-14 11:51:34 -0300

    FEAT adicionando componente header

--- src/RoutesComponent.js
+++ src/RoutesComponent.js
@@ -2,10 +2,12 @@ import React from "react";
 import {BrowserRouter, Routes, Route}  from "react-router-dom";
 import Home from "./pages/Home";
 import Reservas from "./pages/Reservas";
+import Header from "./components/Header";
 
 const RoutesComponent = () =>{
     return(
         <BrowserRouter>
+        <Header/>
             <Routes>
                 <Route path="/" element={<Home/>} />
                 <Route path="/reservas" element={<Reservas/>} />
