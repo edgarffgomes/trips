a876a8c1a1f249cecd21bd0faf5e94a10a66f5be
Edgar Gomes <edgargomes.iff@gmail.com>
2023-02-16 11:26:49 -0300

    FEAT página home atualizada com estilização.

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,4 +1,6 @@
 import React, {useState, useEffect} from 'react';
+import { MdFlightTakeoff} from 'react-icons/md';
+import './style.css';
 import api from '../../services/api';
 export default function Home () {
   const [trips, setTrips] = useState([]);
@@ -13,7 +15,20 @@ export default function Home () {
 
   return (
     <div>
-        <h1>Home</h1>
+        <div className='box'>{trips.map(trip =>(
+            <li key={trip.id}>
+              <img src={trip.image} alt={trip.title}/>
+              <strong>{trip.title}</strong>
+              <span>Status: {trip.status ? 'Disponível' : 'Indisponível'}</span>
+              <button type='button' onClick={()=>{}}>
+                <div>
+                  <MdFlightTakeoff size={16} color="FFF"/>
+                </div>
+                <span>SOLICITAR RESERVA</span>
+              </button>
+            </li>
+        ))}
+        </div>
     </div>
   );
 }
