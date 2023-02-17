0111ac4fc5a939156ad908209fdb3dbf36a5a704
Edgar Gomes <edgargomes.iff@gmail.com>
2023-02-17 09:48:45 -0300

    FEAT criando formatação inicial da página de reservas

--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import './style.css';
+import { MdDelete } from 'react-icons/md';
 
 export default function Reservas () {
   return (
     <div>
-        <h1>Reservas</h1>
+        <h1 className='title'>Você solicitou 1 reserva</h1>
+
+        <div className='reservas'>
+          <img src='https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg' alt='Maceió'/>
+          <strong>Viagem Maceió 7 dias</strong>
+          <span>Quantidade: 1</span>
+          <button type='button' onClick={()=>{}}>
+            <MdDelete size={20} color='#191919'/>
+          </button>
+        </div>
+        <div>
+          <footer>
+            <button>SOLICITAR RESERVAS</button>
+          </footer>
+        </div>
     </div>
   )
 }
