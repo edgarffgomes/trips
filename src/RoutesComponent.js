24bc7c2cfe388150a4189b0dc984e827ded815c2
Edgar Gomes <edgargomes.iff@gmail.com>
2023-02-22 16:11:39 -0300

    FEAT -  Pré-configuração inicial do Redux estabelecida.

--- src/RoutesComponent.js
+++ src/RoutesComponent.js
@@ -1,18 +1,22 @@
 import React from "react";
 import {BrowserRouter, Routes, Route}  from "react-router-dom";
+import { Provider } from 'react-redux';
+import store from './store'
 import Home from "./pages/Home";
 import Reservas from "./pages/Reservas";
 import Header from "./components/Header";
 
 const RoutesComponent = () =>{
     return(
-        <BrowserRouter>
-        <Header/>
-            <Routes>
-                <Route path="/" element={<Home/>} />
-                <Route path="/reservas" element={<Reservas/>} />
-            </Routes>
-        </BrowserRouter>
+        <Provider store={store}>
+            <BrowserRouter>
+            <Header/>
+                <Routes>
+                    <Route path="/" element={<Home/>} />
+                    <Route path="/reservas" element={<Reservas/>} />
+                </Routes>
+            </BrowserRouter>
+        </Provider>
     );
 }
 export default RoutesComponent;
--- src/store/index.js
+++ src/store/index.js
@@ -1,1 +1,4 @@
 import { createStore } from 'redux';
+import rootReducer from './modules/rootReducer'
+const Store =  createStore(rootReducer);
+export default Store;
